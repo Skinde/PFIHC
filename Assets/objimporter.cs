87ba768192946f8ffc93e81b2e91782ffee047ce
Miguel <46155561+Skinde@users.noreply.github.com>
2024-06-28 17:45:57 -0500

    Made functional added feedback

--- Assets/objimporter.cs
+++ Assets/objimporter.cs
@@ -3,52 +3,58 @@ using System.IO;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using Dummiesman;
-
+using JetBrains.Annotations;
+using TMPro;
 
 public class objimporter : MonoBehaviour
 {
     GameObject wrapper;
     string filePath;
     GameObject loadedObject;
-    string URL = "https://s3.amazonaws.com/files.free3d.com/models/2/607102309e9b225e754b2fe2/32-mercedes-benz-gls-580-2020.zip?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA5DEPHINMSI4OS3OO%2F20240618%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240618T221454Z&X-Amz-SignedHeaders=host&X-Amz-Expires=1200&X-Amz-Signature=08b603950904742fefd8a7b97871e011168b33f40b891a2392e952c2aeda6fd2";
-
+    [SerializeField] private TextMeshProUGUI textGUI;
     private void Start()
     {
         filePath = $"{Application.persistentDataPath}/";
-        DownloadFile(URL);
     }
     public void DownloadFile(string url)
     {
+
         string path = GetFilePath(url);
         if (File.Exists(path))
         {
-            Debug.Log("Found file locally, loading...");
-            LoadModel(path);
-            return;
+            Debug.Log("Found file locally, Deleting...");
+            Set_text_status("Found file locally, Deleting...", Color.green);
+            File.Delete(path);
         }
 
+        Set_text_status("Downloading...", Color.green);
         Debug.Log("Before Download");
         StartCoroutine(GetFileRequest(url, (UnityWebRequest req) =>
         {
             if (req.result == UnityWebRequest.Result.ConnectionError || req.result == UnityWebRequest.Result.ProtocolError || req.result == UnityWebRequest.Result.DataProcessingError)
             {
+                Set_text_status("Error: " + $"{req.error} : {req.downloadHandler.text}", Color.red);
                 // Log any errors that may happen
                 Debug.Log($"{req.error} : {req.downloadHandler.text}");
+
             }
             else
             {
                 // Save the model into a new wrapper
-                Debug.Log("File not found downloading...");
+                Debug.Log("Loading...");
+                Set_text_status("Loading...", Color.green);
                 LoadModel(path);
+                Set_text_status("", Color.green);
             }
         }));
     }
 
     string GetFilePath(string url)
     {
-        string[] pieces = url.Split('/');
-        string filename = pieces[pieces.Length - 1];
+        string filename = "fourthtemp.obj";
+        Debug.Log($"{filePath}{filename}");
 
         return $"{filePath}{filename}";
     }
@@ -58,7 +64,6 @@ public class objimporter : MonoBehaviour
         if (loadedObject != null)
             Destroy(loadedObject);
             loadedObject = new OBJLoader().Load(path);
-            loadedObject.transform.SetParent(wrapper.transform);
     }
 
     IEnumerator GetFileRequest(string url, Action<UnityWebRequest> callback)
@@ -69,5 +74,11 @@ public class objimporter : MonoBehaviour
             yield return req.SendWebRequest();
             callback(req);
         }
+    }
+
+    void Set_text_status(string text, Color text_color)
+    {
+        textGUI.text = text; 
+        textGUI.color = text_color;
     }
 }
